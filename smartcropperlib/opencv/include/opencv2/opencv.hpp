4b1d885471f31ec5f1fdf1ddbfbd7c48895ace7e
Pqpo <pqponet@gmail.com>
2017-08-07 13:13:30 +0800

    deleted the unused files

--- smartcropperlib/opencv/include/opencv2/opencv.hpp
+++ smartcropperlib/opencv/include/opencv2/opencv.hpp
@@ -47,7 +47,5 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/ml/ml.hpp"
 
 #endif
